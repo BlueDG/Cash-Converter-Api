264feef3b57e9a620dcce211626c7b6041761be2
BlueDG <Guillaume.dusseuxgicquel@gmail.com>
2019-11-17 23:25:53 +0100

    css

--- src/components/Select.js
+++ src/components/Select.js
@@ -3,7 +3,10 @@ import React from "react";
 export default function Select({ request, setRequest, rates }) {
   return (
     <div>
-      <select onChange={e => setRequest({ ...request, to: e.target.value })}>
+      <select
+        className="select"
+        onChange={e => setRequest({ ...request, to: e.target.value })}
+      >
         {Object.keys(rates).map((rate, i) => {
           return (
             <option key={i} value={rate}>
